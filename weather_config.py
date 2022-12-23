59956c672fe905e111b788d453f39e8a30219ad5
zeroinfinity2 <73826185+zeroinfinity2@users.noreply.github.com>
2022-12-22 20:20:26 -0500

    Create weather_config.py

---
+++ weather_config.py
@@ -0,0 +1,10 @@
+# User configurable variables //////////////////////////////////////////////////////////////////////////////
+
+# Input preferred measurement scale. Options imperial/metric.
+preferred_scale = "imperial"
+
+# Request update timer (in seconds) Minimum is hourly (3600s).
+update_time = 3600
+
+#If you use a VPN, put your actual IP address in the quotes. Otherwise, leave it blank to automatically generate.
+ipaddress = ""
